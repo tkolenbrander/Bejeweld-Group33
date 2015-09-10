0d3e2d27221d21009d1b903a6c2e99445101bef6
welhorst <welhorst@hotmail.nl>
2015-09-10 10:43:04 +0200

    falldowntests

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -1,10 +1,9 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import board.Cell;
 import board.Gem;
-
+import board.GemType;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,7 +16,9 @@ import board.Board;
 public class BoardTest {
   
   private Board board;
-  
+  /**
+   * Sets up a random board for testing purposes.
+   */
   @Before
   public void setUp() {
     board = new Board();
@@ -26,30 +27,31 @@ public class BoardTest {
   /**
    * Tests the falldown method with a single cell missing.
    */
-	@Test
+  @Test
 	@SuppressWarnings("magicnumber")
 	public void falldownSingleChangeTest() {
-	  Cell[][] cells = board.getCells();
-	  Gem gem2 = cells[6][6].getGem();
-	  Gem gem3 = cells[5][6].getGem();
-	  Gem gem4 = cells[4][6].getGem();
-	  Gem gem5 = cells[3][6].getGem();
-	  Gem gem6 = cells[2][6].getGem();
-	  Gem gem7 = cells[1][6].getGem();
-	  Gem gem8 = cells[0][6].getGem();
-	  
-	  board.setCell(null, 6, 7);
+	  Gem bl = new Gem(GemType.BLUE);
+	  Gem wh = new Gem(GemType.WHITE);
+	  Gem gr = new Gem(GemType.GREEN);
+	  Gem pu = new Gem(GemType.PURPLE);
+	  Gem or = new Gem(GemType.ORANGE);
+	  Cell b = new Cell(bl);
+	  Cell w = new Cell(wh);
+	  Cell g = new Cell(gr);
+	  Cell p = new Cell(pu);
+	  Cell o = new Cell(or);
+	  Cell[][] cells = new Cell[][] {{b, w, b, w, b, w, b, w}, {g, p, g, p, g, p, g, p},
+			  {b, w, b, w, b, w, b, w}, {g, p, g, p, g, p, g, p}, {b, w, b, w, b, w, b, w},
+			  {g, p, g, p, g, p, g, p}, {b, w, b, w, b, w, b, o}, {g, p, g, p, g, p, g, null}};
+	  Board board = new Board(cells);
 	  board.falldown();
-	  
-	  assertNotNull(cells[7][6]);
-	  assertEquals(gem2, cells[7][6].getGem());
-	  assertEquals(gem3, cells[6][6].getGem());
-	  assertEquals(gem4, cells[5][6].getGem());
-	  assertEquals(gem5, cells[4][6].getGem());
-	  assertEquals(gem6, cells[3][6].getGem());
-	  assertEquals(gem7, cells[2][6].getGem());
-	  assertEquals(gem8, cells[1][6].getGem());
-	}
+	  cells = board.getCells();
+	  Cell[][] cells2 = new Cell[][] {{b, w, b, w, b, w, b, null}, {g, p, g, p, g, p, g, w},
+			  {b, w, b, w, b, w, b, p}, {g, p, g, p, g, p, g, w}, {b, w, b, w, b, w, b, p},
+			  {g, p, g, p, g, p, g, w}, {b, w, b, w, b, w, b, p}, {g, p, g, p, g, p, g, o}};
+	  Board board2 = new Board(cells2);
+	  assertEquals(board2, board);
+  }
 	
 	/**
 	 * Tests the falldown method with a vertical row of three cells missing.
@@ -57,25 +59,25 @@ public class BoardTest {
 	@Test
 	@SuppressWarnings("magicnumber")
 	public void falldownThreeVerticalTest() {
-	  Cell[][] cells = board.getCells();
-	  Gem gem4 = cells[4][7].getGem();
-	  Gem gem5 = cells[3][7].getGem();
-	  Gem gem6 = cells[2][7].getGem();
-	  Gem gem7 = cells[1][7].getGem();
-	  Gem gem8 = cells[0][7].getGem();
-	  
-	  board.setCell(null, 7, 7);
-	  board.setCell(null, 7, 6);
-	  board.setCell(null, 7, 5);
-	  board.falldown();
-	  
-	  assertNotNull(cells[7][7]);
-	  assertNotNull(cells[6][7]);
-	  assertNotNull(cells[5][7]);
-	  assertEquals(gem4, cells[7][7].getGem());
-	  assertEquals(gem5, cells[6][7].getGem());
-	  assertEquals(gem6, cells[5][7].getGem());
-	  assertEquals(gem7, cells[4][7].getGem());
-	  assertEquals(gem8, cells[3][7].getGem());
-	}
+		  Gem bl = new Gem(GemType.BLUE);
+		  Gem wh = new Gem(GemType.WHITE);
+		  Gem gr = new Gem(GemType.GREEN);
+		  Gem pu = new Gem(GemType.PURPLE);
+		  Cell b = new Cell(bl);
+		  Cell w = new Cell(wh);
+		  Cell g = new Cell(gr);
+		  Cell p = new Cell(pu);
+		  Cell[][] cells = new Cell[][] { {b, w, b, w, b, w, b, w}, {g, p, g, p, g, p, g, p},
+				  {b, w, b, w, b, w, b, w}, {g, p, g, p, g, p, g, p}, {b, w, b, w, b, w, b, w},
+				  {g, p, g, p, g, p, g, null}, {b, w, b, w, b, w, b, null},
+				  {g, p, g, p, g, p, g, null}};
+		  Board board = new Board(cells);
+		  board.falldown();
+		  cells = board.getCells();
+		  Cell[][] cells2 = new Cell[][] {{b, w, b, w, b, w, b, null}, {g, p, g, p, g, p, g, null},
+				  {b, w, b, w, b, w, b, null}, {g, p, g, p, g, p, g, w}, {b, w, b, w, b, w, b, p},
+				  {g, p, g, p, g, p, g, w}, {b, w, b, w, b, w, b, p}, {g, p, g, p, g, p, g, w}};
+		  Board board2 = new Board(cells2);
+		  assertEquals(board2, board);
+	  }
 }
