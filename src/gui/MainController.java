ba8607f4c271e5b575c23a0424911f0bf73db0b9
TH3steven <stevenmeijer9@gmail.com>
2015-09-21 21:00:36 +0200

    Merge master info javafx

---
+++ src/gui/MainController.java
@@ -0,0 +1,20 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController implements Initializable {
+	
+	@FXML private AnchorPane anchorPane;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
