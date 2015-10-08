aaecdfa926181736ad34652ff2c604129edfdd22
TH3steven <stevenmeijer9@gmail.com>
2015-10-08 23:09:01 +0200

    Updates GUI and Player classes with observers

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,9 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
 /**
  * Class for a player. Currently only used for keeping track of his / her
  * score, but in light of possible future developments, it is good to have 
@@ -7,44 +11,93 @@ package game;
  * @author Bart van Oort
  *
  */
-public class Player {
+public class Player implements Score {
 
-  private int score;
-  
-  /**
-   * Creates a new player with 0 as a score.
-   */
-  public Player() {
-    score = 0;
-  }
-  
-  /**
-   * Creates a new player with the given score.
-   */
-  public Player(int newScore) {
-    score = newScore;
-  }
-  
-  /**
-   * Returns the score of the player.
-   * @return score of the player.
-   */
-  public int getScore() {
-    return score;
-  }
-  
-  /**
-   * Adds score to the player's score.
-   * @param add score to be added to the player's score.
-   */
-  public void addScore(int add) {
-    score += add;
-  }
-  
-  /**
-   * Resets the player to its initial state, that is, score = 0..
-   */
-  public void reset() {
-    score = 0;
-  }
+	private int score;
+
+	private List<Observer> observers;
+	private String message;
+	private boolean changed;
+	private final Object MUTEX = new Object();
+
+	/**
+	 * Creates a new player with 0 as a score.
+	 */
+	public Player() {
+		this.observers = new ArrayList<>();
+		score = 0;
+	}
+
+	/**
+	 * Creates a new player with the given score.
+	 */
+	public Player(int newScore) {
+		this.observers = new ArrayList<>();
+		score = newScore;
+	}
+
+	/**
+	 * Returns the score of the player.
+	 * @return score of the player.
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * Adds score to the player's score.
+	 * @param add score to be added to the player's score.
+	 */
+	public void addScore(int add) {
+		score += add;
+	}
+
+	/**
+	 * Resets the player to its initial state, that is, score = 0..
+	 */
+	public void reset() {
+		score = 0;
+	}
+
+	@Override
+	public void register(Observer obj) {
+		if(obj == null) throw new NullPointerException("Null Observer");
+		synchronized (MUTEX) {
+			if(!observers.contains(obj)) {
+				observers.add(obj);
+			};
+		}
+	}
+
+	@Override
+	public void unregister(Observer obj) {
+		synchronized (MUTEX) {
+			observers.remove(obj);
+		}
+	}
+
+	@Override
+	public void notifyObservers() {
+		List<Observer> observersLocal = null;
+
+		synchronized (MUTEX) {
+			if (!changed) {
+				return;
+			}
+			
+			observersLocal = new ArrayList<>(this.observers);
+			this.changed = false;
+		}
+		
+		for (Observer obj : observersLocal) {
+			obj.update(null, MUTEX); //?
+		}
+
+	}
+
+	@Override
+	public Object getUpdate(Observer obj) {
+		return this.message;
+	}
+
 }
--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,9 +1,13 @@
 package gui;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import game.Game;
 import game.LoadGame;
 import game.Logger;
 import game.SaveGame;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -22,16 +26,16 @@ import javafx.stage.Stage;
  * 
  * @author Steven Meijer & Thomas Kolenbrander
  */
-public class GUI extends Application {
+public class GUI extends Application implements Observer {
 
 	/**
 	 * X and Y dimensions of the board.
 	 */
 	private static final int WINDOW_X = 520;
 	private static final int WINDOW_Y = 620;
-	
-	private static Game game;
+
 	private BoardPane boardPane;
+	private static Game game;
 	private static Label scoreLabel;
 	private static Label errorLabel;
 	private static GUI gui;
@@ -43,6 +47,7 @@ public class GUI extends Application {
 	protected static Game getGame() {
 		return game;
 	}
+	
 	/**
 	 * returns the gui.
 	 * @return gui
@@ -50,6 +55,7 @@ public class GUI extends Application {
 	protected static GUI getgui() {
 		return gui;
 	}
+	
 	/**
 	 * Main class to launch the application.
 	 * 
@@ -70,12 +76,16 @@ public class GUI extends Application {
 	@SuppressWarnings("magicnumber")
 	public void start(Stage stage) {
 		Logger.logInfo("Game started");	
+
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(GUI.class.getResource("/assets/config/gui.fxml"));
+
 		BorderPane borderPane = new BorderPane();
 		Scene scene = new Scene(borderPane, WINDOW_X, WINDOW_Y);	
-		scoreLabel = new Label("Score: ");
-		scoreLabel.setFont(new Font("Arial", 22));		
+
+		scoreLabel = new Label("Score: 0");
+		scoreLabel.setFont(new Font("Arial", 22));
+		
 		errorLabel = new Label("");
 		errorLabel.setFont(new Font("Arial", 22));
 		boardPane = new BoardPane();
@@ -84,13 +94,14 @@ public class GUI extends Application {
 		borderPane.setTop(hbox);
 		borderPane.setCenter(boardPane.getBoardPane());		
 		borderPane.setBottom(errorPanel);
-		
+
 		// Properties of the stage
 		stage.setTitle("SwekJeweled");
 		stage.setResizable(false);
 		stage.centerOnScreen();
 		stage.setScene(scene);
 		stage.show();
+
 		Logger.logInfo("Game succesfully initialized");
 	}
 
@@ -104,30 +115,38 @@ public class GUI extends Application {
 		hbox.setPadding(new Insets(15, 12, 15, 150));
 		hbox.setSpacing(10);
 		hbox.setStyle("-fx-background-color: #C0C0C0;");
+
 		Button buttonSave = new Button("Save Game");
 		buttonSave.setPrefSize(100, 20);
 		Button buttonLoad = new Button("Load Game");
 		buttonLoad.setPrefSize(100, 20);
+
 		buttonSave.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                SaveGame.save(game);
-                setError("Game saved!"); } });
+			@Override
+			public void handle(ActionEvent event) {
+				SaveGame.save(game);
+				setError("Game saved!"); } });
+
 		buttonLoad.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-              try {
-                game = LoadGame.load();
-                game.start();
-                boardPane.refresh();
-                setScore(GUI.game.getPlayer().getScore());
-                setScore(GUI.game.getPlayer().getScore());
-                setError("Game loaded!"); }
-              catch (Exception e) {
-                setError("Cannot load game!"); } } });
-		setScore(0);
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					game = LoadGame.load();
+					game.start();
+					boardPane.refresh();
+					//setScore(GUI.game.getPlayer().getScore());
+					setError("Game loaded!"); }
+				catch (Exception e) {
+					setError("Cannot load game!"); 
+				} 
+			} 
+		});
+
+		//setScore(0);
 		hbox.getChildren().addAll(scoreLabel, buttonSave, buttonLoad);
-		return hbox;  }
+
+		return hbox;  
+	}
 
 	/**
 	 * Adds the error panel at the bottom of the screen.
@@ -139,22 +158,24 @@ public class GUI extends Application {
 		hbox.setPadding(new Insets(15, 12, 15, 150));
 		hbox.setSpacing(10);
 		hbox.setStyle("-fx-background-color: #C0C0C0;");
-		
+
 		Button buttonRestart = new Button("Restart Game");
 		buttonRestart.setPrefSize(100, 20);
-		
+
 		buttonRestart.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-            	game.reset();
-            	game.start();
-            	boardPane.refresh();
-            }
-        });
-		
+			@Override
+			public void handle(ActionEvent event) {
+				game.reset();
+				game.start();
+				boardPane.refresh();
+			}
+		});
+
 		hbox.getChildren().addAll(errorLabel, buttonRestart);
+
 		return hbox;
 	}
+	
 	/**
 	 * sets a message for the error label.
 	 * @param s the message to be displayed
@@ -162,13 +183,25 @@ public class GUI extends Application {
 	public void setError(String s) {
 		errorLabel.setText(s);
 	}
+	
 	/**
 	 * sets a number for the score label.
 	 * @param score number to be displayed
 	 */
-	public void setScore(int score) {
+	public static void setScore(int score) {
 		String s = "Score: " + score;
 		scoreLabel.setText(s);
 	}
-	
+
+	/**
+	 * Sets a number for the score label.
+	 * @param o The observable object
+	 * @param arg The argument.
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		String s = "Score: " + arg;
+		scoreLabel.setText(s);
+	}
+
 }
